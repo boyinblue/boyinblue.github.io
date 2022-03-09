e7ea9c85e41a12dbc8b93921e890afb120bb4da9
boyinblue <boyinblue@naver.com>
2022-03-09 22:41:38 +0900

    restore sitemap

--- build/make_site_map.sh
+++ build/make_site_map.sh
@@ -14,7 +14,7 @@ rm -rf "$SITEMAP_TXT_FILE_TMP"
 function print_header()
 {
   echo '<?xml version="1.0" encoding="UTF-8"?>'
-  echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+  echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">'
 }
 
 function print_tail()
@@ -32,11 +32,11 @@ function print_list_xml()
       for file in ${files[@]}
       do
         lastmod=$(date +"%m-%d-%YT%H:%M:%S%:z" -r ${file})
-	echo "<url>"
-        echo "<loc>${HOMEPAGE_URL}/${file/.md/.html}</loc>"
-	echo "<lastmod>${lastmod}</lastmod>"
-	echo "<changefreq>weekly</changefreq>"
-	echo "</url>"
+	echo "  <url>"
+        echo "    <loc>${HOMEPAGE_URL}/${file/.md/.html}</loc>"
+#	echo "    <lastmod>${lastmod}</lastmod>"
+#	echo "    <changefreq>weekly</changefreq>"
+	echo "  </url>"
       done
     fi
   done
@@ -65,12 +65,20 @@ print_tail >> ${SITEMAP_XML_FILE_TMP}
 print_list_txt >> ${SITEMAP_TXT_FILE_TMP}
 popd
 
-diff=$(diff $SITEMAP_XML_FILE_TMP $SITEMAP_XML_FILE)
-if [ "${diff}" != "" ]; then
+if [ ! -e ${SITEMAP_XML_FILE} ]; then
   cp ${SITEMAP_XML_FILE_TMP} ${SITEMAP_XML_FILE}
+else
+  diff=$(diff $SITEMAP_XML_FILE_TMP $SITEMAP_XML_FILE)
+  if [ "${diff}" != "" ]; then
+    cp ${SITEMAP_XML_FILE_TMP} ${SITEMAP_XML_FILE}
+  fi
 fi
 
-diff=$(diff $SITEMAP_TXT_FILE_TMP $SITEMAP_TXT_FILE)
-if [ "${diff}" != "" ]; then
+if [ ! -e ${SITEMAP_TXT_FILE} ]; then
   cp ${SITEMAP_TXT_FILE_TMP} ${SITEMAP_TXT_FILE}
+else
+  diff=$(diff $SITEMAP_TXT_FILE_TMP $SITEMAP_TXT_FILE)
+  if [ "${diff}" != "" ]; then
+    cp ${SITEMAP_TXT_FILE_TMP} ${SITEMAP_TXT_FILE}
+  fi
 fi
