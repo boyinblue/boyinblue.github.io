975b208137011a02e321d25f8f710c759ba146f6
boyinblue <boyinblue@naver.com>
2022-03-23 14:45:16 +0900

    [UPBit] auto generated site map & main page

--- 009_upbit/_build/auto.sh
+++ 009_upbit/_build/auto.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
-
-TMP_DIFF_FILE="tmp/diff.txt"
-
-mkdir -p tmp
+set -e
 
 git pull
 
-./make_site_map.sh
-./make_readme.sh
-
-diff=$(git diff)
-if [ "${diff}" != "" ]; then
-  git add ../sitemap.xml
-  git add ../sitemap.txt
-  git add ../README.md
-  git commit -m "[Index] auto generated site map & main page"
-  git push origin main
-
-  echo "<pre><code>" > ${TMP_DIFF_FILE}
-  echo "${diff}" >> ${TMP_DIFF_FILE}
-  echo "</code></pre>" >> ${TMP_DIFF_FILE}
-  ./send_email.sh ${TMP_DIFF_FILE}
-fi
+python3 002_upbit_get_daily_candle.py
+git add ..
+git commit -m "[UPBit] auto generated site map & main page"
+git push origin main
