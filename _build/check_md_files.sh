7aff52bef85fb92b4e798402d1922e21fed779b1
boyinblue <boyinblue@naver.com>
2022-03-27 21:19:12 +0900

    check_md_files.sh

--- _build/check_md_files.sh
+++ _build/check_md_files.sh
@@ -5,11 +5,52 @@ OUTPUT="${CWD_DIR}/tmp/md_check_report.txt"
 
 HOMEPAGE_URL="https://boyinblue.github.io"
 
-rm -rf "$SITEMAP_TXT_FILE_TMP"
-
 function check_md_file()
 {
   filename=${1}
+  
+#  echo "$(pwd)/${filename}"
+
+  i=0
+  while read line || [ -n "$line" ]
+  do
+#    echo "Line $i: $line"
+    arrLine[${i}]="$line"
+    ((i+=1))
+  done < ${filename}
+
+#  echo "$i lines read"
+#  echo "${arrLine[@]}"
+#  echo "${arrLine[0]}"
+#  echo "${arrLine[1]}"
+#  echo "${arrLine[2]}"
+#  echo "${arrLine[3]}"
+
+  if [[ "${arrLine[0]}" != "---"* ]]; then
+    echo "$(pwd)/${filename}"
+    echo "Files does not start with YAML header"
+	echo "${arrLine[0]}"
+	echo ""
+	return 1
+  elif  [[ "${arrLine[1]}" != "title: "* ]]; then
+    echo "$(pwd)/${filename}"
+    echo "YAML header does not start with title information"
+	echo "${arrLine[1]}"
+	echo ""
+	return 2
+  elif  [[ "${arrLine[2]}" != "description: "* ]]; then
+    echo "$(pwd)/${filename}"
+    echo "YAML header does not start with description"
+	echo "$arrLine[2]"
+	echo ""
+	return 2
+  elif  [[ "${arrLine[3]}" != "---"* ]]; then
+    echo "$(pwd)/${filename}"
+    echo "YAML header does not end with '---'"
+	echo "${arrLine[3]}"
+	echo ""
+	return 2
+  fi
 }
 
 function iterate()
@@ -17,16 +58,22 @@ function iterate()
   files=$(ls)
   for file in ${files[@]}
   do
-    if [ -d "$file" ]; then
-      echo "Check Dir : $file"
+    if [ "$file" == "." ] || [ "$file" == ".." ]; then
+	  continue
+	elif [ -d "$file" ]; then
+#      echo "Check Dir : $file"
+	  pushd $file >> /dev/null
+	    iterate
+	  popd >> /dev/null
 	elif [ "${file:(-3)}" == ".md" ]; then
-	  echo "MD File : $file"
-	else
-	  echo "Skip : $file"
+#	  echo "MD File : $file"
+	  check_md_file "$file"
+#	else
+#	  echo "Skip : $file"
 	fi
   done
 }
 
 pushd ..
-check_md_files
+iterate
 popd
