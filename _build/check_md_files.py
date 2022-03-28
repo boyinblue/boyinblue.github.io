04c2d60093e7f6a28d20089dd05c3cae5050d2f3
boyinblue <boyinblue@naver.com>
2022-03-28 16:22:16 +0900

    update

--- _build/check_md_files.py
+++ _build/check_md_files.py
@@ -1,33 +1,39 @@
 import os
 from bs4 import BeautifulSoup
 
+def add_line_into_body(yaml, line):
+    if yaml["BODY"] == "" && line == "\n":
+        return
+    
+    yaml["BODY"] = yaml["BODY"] + line
+
 def read_yaml_header(arrLines, yaml):
     for line in arrLines:
         parsing_done = False
 
         # YAML 헤더 파싱이 모두 끝났다면 나머지 내용은 본문에 넣는다.
         if yaml["YAML_END"] == "---\n":
-            yaml["BODY"] = yaml["BODY"] + line
+            add_line_into_body(yaml, line)
             continue
 
         # "---" 문자열이 나오면 YAML START인지 YAML END인지 판별한다.
         if line == "---\n":
             if yaml["YAML_START"] == "":
-                print("YAML header started!")
+#                print("YAML header started!")
                 yaml["YAML_START"] = line
                 continue
             elif yaml["YAML_END"] == "":
-                print("YAML header end!")
+#                print("YAML header end!")
                 yaml["YAML_END"] = line
                 continue
-            else
-                yaml["BODY"] = yaml["BODY"] + line
+            else:
+                add_line_into_body(yaml, line)
                 continue
 
         # YAML 헤더를 파싱한다.
         for key in yaml.keys():
             if line.startswith(key):
-                if not yaml_flag:
+                if yaml["YAML_START"] != "---\n":
                     # "---" 없이 YAML 헤더가 시작됨
                     print("Invalid YAML header :", line)
                     return 1
@@ -41,7 +47,7 @@ def read_yaml_header(arrLines, yaml):
 #            print("Not defined YAML header :", line)
             yaml["YAML_START"] = "---\n"
             yaml["YAML_END"] = "---\n"
-            yaml["BODY"] = yaml["BODY"] + line
+            add_line_into_body(yaml, line)
 
     return 0
 
@@ -71,7 +77,7 @@ def check_md_file(filename):
             "title: " : "",
             "description: " : "",
             "YAML_END" : "",
-            "PADDING" : "\n\n"
+            "PADDING" : "\n\n",
             "BODY" : "" }
 
     cnt = 0
@@ -97,7 +103,7 @@ def iterate_directory(dir):
     files = os.listdir(dir)
     for file in files:
         path = "{}/{}".format(dir, file)
-        print( "file : {}".format(path) )
+#        print( "file : {}".format(path) )
         if file.endswith(".md"):
 #            print("  Check md file")
             check_md_file(path)
