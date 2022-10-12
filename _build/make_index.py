0effa5e9589ab84933d47b7750ae78b765a30f98
boyinblue <boyinblue@naver.com>
2022-10-13 00:12:38 +0900

    update

--- _build/make_index.py
+++ _build/make_index.py
@@ -2,38 +2,14 @@
 
 import os
 
-def replace_index_to_readme(path):
-  f_rd = open(path, "r")
-  lines = f_rd.readlines()
-  f_rd.close()
-
-  f_wr = open(path, "w")
-  for line in lines:
-    line2 = line.replace("index.html", "README.html")
-    f_wr.write(line2)
-  f_wr.close()
-
-def replace_readme_to_index(path):
-  f_rd = open(path, "r")
-  lines = f_rd.readlines()
-  f_rd.close()
-
-  f_wr = open(path, "w")
-  for line in lines:
-    line2 = line.replace("README.html", "index.html")
-    f_wr.write(line2)
-  f_wr.close()
-
 def iterate_dir(dir):
   print("PATH :", dir)
 
   files = os.listdir(dir)
   for file in files:
     file_path = dir + "/" + file
-    if file == "README.md":
+    if file == "_README.md":
       os.remove(file_path)
-#      replace_readme_to_index(file_path)
-#      os.rename(file_path, dir + "/index.md")
     elif os.path.isdir(file_path):
       iterate_dir(file_path)
 
