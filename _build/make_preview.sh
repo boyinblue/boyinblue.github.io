134b24e80af7ccae2edd5d0d191c3512cac819fc
boyinblue <boyinblue@naver.com>
2022-11-01 09:16:47 +0900

    update

--- _build/make_preview.sh
+++ _build/make_preview.sh
@@ -19,14 +19,18 @@ IFS=$'\n'
 for line in ${lines}
 do
   if [[ "${line}" == *"og:title"* ]]; then
-    title="${line##*content=\"}"
-    title="${title%\"*}"
-  elif [[ "${line}" == *"og:description"* ]]; then
-    description="${line##*content=\"}"
-    description="${description%\"*}"
-  elif [[ "${line}" == *"og:image"* ]]; then
-    image="${line##*content=\"}"
-    image="${image%\"*}"
+    title="${line##*og:title\" content=\"}"
+    title="${title%%\"*}"
+  fi
+  
+  if [[ "${line}" == *"og:description"* ]]; then
+    description="${line##*og:description\" content=\"}"
+    description="${description%%\"*}"
+  fi
+  
+  if [[ "${line}" == *"og:image"* ]]; then
+    image="${line##*og:image\" content=\"}"
+    image="${image%%\"*}"
   fi
 done
 IFS=$IFS_OLD
