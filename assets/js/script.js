189542b37634b87eef6c6889b6d4e4c1e92ae940
boyinblue <boyinblue@naver.com>
2022-11-03 14:33:14 +0900

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const link_info = document.getElementById('link_info');
 document.addEventListener("mousemove", (e) => { // mousemove이벤트를 이용해 움
     // 마우스의 좌표는 clientX와 clientY를 이용해 알수 있다. -> 브라우저 window의 좌표값 위치를 전달한다.
     // pageX, pageY와는 다름.
-    const mouseX = e.pageX;
+    const mouseX = e.pageX + 10;
     const mouseY = e.pageY;
     link_info.style.left = mouseX + 'px';
     link_info.style.top = mouseY + 'px';
