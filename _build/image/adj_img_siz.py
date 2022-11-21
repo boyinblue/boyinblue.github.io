a63b04d58568a2931a32063a9c507013c38910a5
boyinblue <boyinblue@naver.com>
2022-11-21 10:39:47 +0900

    [이벤트 정보] _pages가 아닌 _posts에 저장되도록 변경함

--- _build/image/adj_img_siz.py
+++ _build/image/adj_img_siz.py
@@ -4,23 +4,38 @@ import os
 import glob
 from PIL import Image
 
-IMAGE_PATH = "../../assets/images/*"
+IMAGE_PATH = "../../assets/images"
 
-def iterage_dir(path):
-    files = os.
+def iterate_dir(dir):
+    print(dir)
+    files = os.listdir(dir)
 
-files = glob.glob(IMAGE_PATH)
+    for file in files:
+        path = dir + "/" + file
+        if file == "." or file == "..":
+            continue
+        elif os.path.isdir(path):
+            iterate_dir(path)
+        else:
+            check_image(path)
 
-for f in files:
-    try:
-        img = Image.open(f)
-    except OSError as e:
-        continue
-
-    if img.width > 1000:
-        print("{} (W:{}, H:{})".format(f, img.width, img.height))
-        ret = input("Do you want to resize? (y/n) ")
-
-    #img_resize = img.resize((int(img.width / 2), int(img.height / 2)))
-:    #title, ext = os.path.splitext(f)
+def resize_image(img, path):
+    img_resize = img.resize((int(img.width / 2), int(img.height / 2)))
+    img_resize.save(path)
+    #title, ext = os.path.splitext(f)
     #img_resize.save(title + '_half' + ext)
+
+def check_image(path):
+    try:
+        img = Image.open(path)
+    except OSError as e:
+        return
+
+    
+    if img.width > 1000:
+        print("{} (W:{}, H:{})".format(path, img.width, img.height))
+        ret = input("Do you want to resize? (y/n) ")
+        resize_image(img, path)
+
+if __name__ == '__main__':
+    iterate_dir(IMAGE_PATH)
